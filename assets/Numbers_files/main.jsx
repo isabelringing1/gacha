fa254539b0dbb05db0e476255f24a1df24e5d6af
Isabel Lee <isabelringing1@gmail.com>
2026-01-13 19:04:21 -0500

    Updates

---
+++ assets/Numbers_files/main.jsx
@@ -0,0 +1,10 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=fab24a35"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=fab24a35"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.jsx?t=1767556994870";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/Users/isabellee/Dropbox/addicting-game/src/main.jsx",
+  lineNumber: 4,
+  columnNumber: 52
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBR21EO0FBSG5ELFNBQVNBLGtCQUFrQjtBQUMzQixPQUFPQyxTQUFTO0FBRWhCRCxXQUFXRSxTQUFTQyxlQUFlLE1BQU0sQ0FBQyxFQUFFQyxPQUFPLHVCQUFDLFNBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUFJLENBQUciLCJuYW1lcyI6WyJjcmVhdGVSb290IiwiQXBwIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsInJlbmRlciJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlcyI6WyJtYWluLmpzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSBcInJlYWN0LWRvbS9jbGllbnRcIjtcbmltcG9ydCBBcHAgZnJvbSBcIi4vQXBwLmpzeFwiO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKSkucmVuZGVyKDxBcHAgLz4pO1xuIl0sImZpbGUiOiIvVXNlcnMvaXNhYmVsbGVlL0Ryb3Bib3gvYWRkaWN0aW5nLWdhbWUvc3JjL21haW4uanN4In0=
